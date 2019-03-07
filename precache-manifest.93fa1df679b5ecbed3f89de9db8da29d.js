544d42544a53d629cf6047fd84cad241b4c32120
Eric Vazquez <ericvazquez@Erics-MBP.attlocal.net>
2019-03-06 21:29:34 -0500

    Updates

---
+++ precache-manifest.93fa1df679b5ecbed3f89de9db8da29d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1039d7c384f535d3af2f",
+    "url": "/simple-react-counter/static/js/runtime~main.1d14972d.js"
+  },
+  {
+    "revision": "9525c458cf5c0a4276e7",
+    "url": "/simple-react-counter/static/js/main.633dfaed.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/simple-react-counter/static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "9525c458cf5c0a4276e7",
+    "url": "/simple-react-counter/static/css/main.6b8be019.chunk.css"
+  },
+  {
+    "revision": "74f4f003c7df628bea3d7012cf590b34",
+    "url": "/simple-react-counter/index.html"
+  }
+];
